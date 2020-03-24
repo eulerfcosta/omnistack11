d94441db4a680d7714a03bef6ff9a6f4a86d3ee8
eulerfcosta <euler.dacosta@gmail.com>
2020-03-24 20:20:16 -0300

    c011 - correção do nome de coluna na base de dados

--- backend/src/database/migrations/20200324114821_create_incidents.js
+++ backend/src/database/migrations/20200324114821_create_incidents.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('incidents', function(table) {
         table.increments();
         table.string('title', 200).notNullable();
-        table.string('decription').notNullable();
+        table.string('description').notNullable();
         table.decimal('value').notNullable();
         table.string('ong_id').notNullable();
 
