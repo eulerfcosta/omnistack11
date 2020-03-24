a02eb9d73521d393d78322155872b858e00ca5ec
eulerfcosta <euler.dacosta@gmail.com>
2020-03-24 03:39:23 -0300

    c003 - Arquivo index.js  da aplicação Backend. modelo básioc para testar node.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
 
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({
+        evento: 'Semana Omnistack 11.0',
+        aluno: 'Euler Costa'
+    });
+});
+
+app.listen(4001);
