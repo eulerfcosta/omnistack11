6018f3ffcaac55526c215c30be9e25acacfb4bb9
eulerfcosta <euler.dacosta@gmail.com>
2020-03-24 20:31:56 -0300

    c013 - Criando Cortroller Profile. lisat os incedents de uma Ong especifica

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
 
 
 const routes = express.Router();
@@ -16,5 +17,6 @@ routes.get('/incidents/:id', IncidentController.show);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
 routes.put('/incidents/:id', IncidentController.update);
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
