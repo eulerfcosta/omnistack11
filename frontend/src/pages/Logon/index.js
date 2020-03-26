e40e6c8218347e846c5fff76a4a22851bb0fed04
eulerfcosta <euler.dacosta@gmail.com>
2020-03-25 22:38:49 -0300

    c023 - Implementado funcionalidade de Login ni Frontend.

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React , { useState }from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+
+import api from '../../services/api';
+
 import './styles.css';
 
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
 export default function Logon() {
+    const [ id, setId ] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e){
+        e.preventDefault();
+
+        try {
+            console.log({ id});
+            const response = await api.post('/sessions' , { id });
+            localStorage.setItem('ongId' , id);
+            localStorage.setItem('ongName' , response.data.name);
+            history.push('/profile');
+        } catch (err) {
+            alert("Falha no Login");
+        }
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be The Heroe" />
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça se logon</h1>
-                    <input placeholder="Sua ID" />
+                    <input placeholder="Sua ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                        />
                     <button className="button" type="submit">Entrar</button>
                     <Link className="back-link" to="/register">
                         <FiLogIn size={16} color='#E02041' />
